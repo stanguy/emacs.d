f04ffe208d0bbdacf3c91a71253659e3e68c40da
Sebastien Tanguy <seb@dthg.net>
2012-01-11 20:13:53 +0100

    Misc changes + window numbering

--- init.el
+++ init.el
@@ -200,8 +200,59 @@
 
 (add-hook 'find-file-root-hook 'find-file-root-header-warning)
 
+(windmove-default-keybindings 'meta)
+
+
+ 
+(defun color-theme-twilight ()
+  "Color theme by Marcus Crafter, based off the TextMate Twilight theme, created 2008-04-18"
+  (interactive)
+  (color-theme-install
+'(color-theme-twilight
+((background-color . "black")
+(background-mode . dark)
+(border-color . "black")
+(cursor-color . "#A7A7A7")
+(foreground-color . "#F8F8F8")
+(mouse-color . "sienna1"))
+(default ((t (:background "black" :foreground "white"))))
+(blue ((t (:foreground "blue"))))
+(bold ((t (:bold t))))
+(bold-italic ((t (:bold t))))
+(border-glyph ((t (nil))))
+(buffers-tab ((t (:background "black" :foreground "white"))))
+(font-lock-builtin-face ((t (:foreground "white"))))
+(font-lock-comment-face ((t (:italic t :foreground "#5F5A60"))))
+(font-lock-constant-face ((t (:foreground "#CF6A4C"))))
+(font-lock-doc-string-face ((t (:foreground "DarkOrange"))))
+(font-lock-function-name-face ((t (:foreground "#9B703F"))))
+(font-lock-keyword-face ((t (:foreground "#CDA869"))))
+(font-lock-preprocessor-face ((t (:foreground "Aquamarine"))))
+(font-lock-reference-face ((t (:foreground "SlateBlue"))))
+ 
+(font-lock-regexp-grouping-backslash ((t (:foreground "#E9C062"))))
+(font-lock-regexp-grouping-construct ((t (:foreground "red"))))
+ 
+(font-lock-string-face ((t (:foreground "#8F9D6A"))))
+(font-lock-type-face ((t (:foreground "#9B703F"))))
+(font-lock-variable-name-face ((t (:foreground "#7587A6"))))
+(font-lock-warning-face ((t (:bold t :background "#EE799F" :foreground "red"))))
+(gui-element ((t (:background "#D4D0C8" :foreground "black"))))
+(region ((t (:background "#27292A"))))
+(mode-line ((t (:background "grey75" :foreground "black"))))
+(highlight ((t (:background "#111111"))))
+(highline-face ((t (:background "SeaGreen"))))
+(italic ((t (nil))))
+(left-margin ((t (nil))))
+(text-cursor ((t (:background "yellow" :foreground "black"))))
+(toolbar ((t (nil))))
+(underline ((nil (:underline nil))))
+(zmacs-region ((t (:background "snow" :foreground "blue")))))))
+
+
 ;(set-default-font "Bitstream Vera Sans Mono-11")
-(set-default-font "DejaVu Sans Mono-11")
+;(set-default-font "DejaVu Sans Mono-11")
+(set-default-font "Inconsolata-12")
 (set-face-background 'mmm-default-submode-face "#6e625a")
 
 (custom-set-variables
@@ -209,7 +260,7 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(safe-local-variable-values (quote ((folded-file . t) (fold-internal-margins)))))
+ '(safe-local-variable-values (quote ((fold-internal-margins) (folded-file . t)))))
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
@@ -217,4 +268,6 @@
   ;; If there is more than one, they won't work right.
  )
 
+(require 'window-numbering)
+(window-numbering-mode 1)
 (put 'upcase-region 'disabled nil)
