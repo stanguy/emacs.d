3d59f9dd7932a7906d41677b45ca7794a87e84e3
Sebastien Tanguy <seb@dthg.net>
2013-12-03 23:13:38 +0100

    Force default font on X

--- 24/02system.el
+++ 24/02system.el
@@ -3,6 +3,9 @@
   (setq mac-option-modifier nil)
   (set-default-font "-apple-Menlo-medium-normal-normal-*-12-*-*-*-m-0-fontset-auto5")
 )
+(when (eq window-system 'x)
+  (set-default-font "Inconsolata-12")
+)
 
 (setq make-backup-files nil)
 
