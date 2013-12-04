699038bb1ac81843d8e40696155f39997c5bf774
Sebastien Tanguy <seb@dthg.net>
2013-12-04 10:03:11 +0100

    Add programming modes configuration

--- 24/04prog.el
+++ 24/04prog.el
@@ -7,6 +7,16 @@
 ;; Press Command-b for fuzzy switch buffer
 (global-set-key (kbd "s-b") 'projectile-switch-to-buffer)
 
+(setq-default indent-tabs-mode nil)
+; runs make in the "current directory"
+(global-set-key "\C-xm" 'compile)
+; Goes to the next error in the compile errors list...
+; or it goes to the next grep match in the grep match list
+(global-set-key "\C-x\C-j" 'next-error)
+;; Set the size of the compilation window height
+(setq compilation-window-height 15)
+;;force compilation to scroll
+(setq compilation-scroll-output t)
 
 (defalias 'perl-mode 'cperl-mode)
 (setq-default
@@ -18,6 +28,45 @@
  cperl-mode-map nil
  cperl-extra-newline-before-brace nil
  )
+
+(defconst my-c-style
+       '((c-tab-always-indent        . t)
+         (c-comment-only-line-offset . 0)
+         (c-hanging-braces-alist     . ((substatement-open before after)
+                                        (brace-list-open)))
+         (c-hanging-colons-alist     . ((member-init-intro before)
+                                        (inher-intro)
+                                        (case-label after)
+                                        (label after)
+                                        (access-label after)))
+         (c-cleanup-list             . (scope-operator
+                                        empty-defun-braces
+                                        defun-close-semi))
+         (c-offsets-alist            . ((arglist-close . c-lineup-arglist)
+                                        (substatement-open . 0)
+                                        (case-label        . 4)
+                                        (block-open        . 0)
+                                        (knr-argdecl-intro . -)))
+         (c-echo-syntactic-information-p . t))
+       "My C Programming Style")
+
+;; Customizations for all modes in CC Mode.
+(defun my-c-mode-common-hook ()
+  ;; add my personal style and set it for the current buffer
+  (c-add-style "PERSONAL" my-c-style t)
+  ;; other customizations
+  ;; we like auto-newline and hungry-delete
+  (c-toggle-auto-hungry-state 1)
+  ;; key bindings for all supported languages.  We can put these in
+  ;; c-mode-base-map because c-mode-map, c++-mode-map, objc-mode-map,
+  ;; java-mode-map, idl-mode-map, and pike-mode-map inherit from it.
+  (define-key c-mode-base-map "\C-m" 'c-context-line-break))
+
+(add-hook 'c-mode-common-hook
+                  'my-c-mode-common-hook)
+(add-hook 'c++-mode-hook (lambda () (hs-minor-mode 1)))
+(add-hook 'php-mode-hook (lambda () (c-set-style "symfony2")))
+(global-cwarn-mode 1)
 
 (setq c-default-style
       '((php-mode . "symfony2")
